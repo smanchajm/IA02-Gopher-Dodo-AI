058adf01fd72bf7cb15c33bdedb3c3863845d18d
smanchajm <samuel.manchajm@gmail.com>
2024-05-05 20:29:11 +0200

    Générateur de grille vide

---
+++ hexagonal_board.py
@@ -0,0 +1,48 @@
+# Generated code -- CC0 -- No Rights Reserved -- http://www.redblobgames.com/grids/hexagons/
+from __future__ import division
+from __future__ import print_function
+import collections
+import math
+from typing import List
+
+# 0 représente une case vide, 1 représente une case marquée par rouge et 2 une case marquée par bleu
+Grid = tuple[tuple[int, ...], ...]
+
+
+def grid_tuple_to_grid_list(grid: Grid) -> list[list[int]]:
+    return [list(i) for i in grid]
+
+
+def grid_list_to_grid_tuple(grid: list[list[int]]) -> Grid:
+    return tuple(tuple(i) for i in grid)
+
+
+def grid_generation(n: int):
+    # Création d'un tableau 2n * 2n
+    grid = [[-1] * (2 * n) for _ in range(2 * n)]
+
+    # Remplissage d'une grille vide
+    for r in range(n):
+        for q in range(n - r, 2*n):
+            grid[r][q] = 0
+    for r in range(n):
+        for q in range(2*n-r-1):
+            grid[r+n][q] = 0
+
+    grid.insert(n, [-0] * (2 * n))
+    for ligne in grid:
+        ligne.insert(n, 0)
+
+    for ligne in grid:
+        print(ligne)
+    return grid
+
+
+def main():
+    grid_generation(3)
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
