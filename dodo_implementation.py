6c13a3d4142218d1a808afbbb2506747d9f23d4b
smanchajm <samuel.manchajm@gmail.com>
2024-05-16 18:03:00 +0200

    nettoyage

--- dodo_implementation.py
+++ dodo_implementation.py
@@ -2,6 +2,8 @@
 import random
 from typing import List, Set, Callable, Union
 import hexagonal_board as hexa
+from grid import *
+
 
 # Structures de données
 Grid = hexa.Grid
@@ -39,42 +41,12 @@ player1 = 1  # Player bleu
 player2 = 2  # Player rouge
 
 
-# Fonction de génération de grille pour le jeu DoDo --> en cours de réalisation
-def grid_generation_dodo(n: int) -> Grid:
-    # Création d'un tableau 2(n-1) * 2(n-1)
-    m = n - 1
-    grid = [[-1] * (2 * m) for _ in range(2 * m)]
-
-    # Remplissage d'une grille vide
-    # Remplissage de la première moitié de la grille
-    for r in range(m):
-        for q in range(m - r, 2 * m):
-            if r < m - 1 and q >= m:
-                grid[r][q] = 1
-            else:
-                grid[r][q] = 0
-    # Remplissage de la seconde moitié de la grille
-    for r in range(m):
-        for q in range(2 * m - r - 1):
-            if r >= 1 and q < m:
-                grid[r + m][q] = 2
-            else:
-                grid[r + m][q] = 0
-    # Ajout d'une colonne et d'une ligne de 0 au milieu de la grille
-    grid.insert(m, [-0] * (2 * m))
-    for ligne in grid:
-        ligne.insert(m, 0)
-
-    return hexa.grid_list_to_grid_tuple(grid)
-
-
 # Règles du DoDo
 
 
 # Fonction retournant les actions possibles d'un joueur pour un état donné (voir optimisation)
 def legals_dodo(grid: Grid, player: Player, directions) -> list[ActionDodo]:
     actions: Set[ActionDodo] = set()  # On utilise un ensemble pour garantir l'unicité
-    temp_grid: list[list[int, ...], ...] = hexa.grid_tuple_to_grid_list(grid)
 
     # On parcourt l'ensemble des cases de la grille
     for i, ligne in enumerate(grid):
@@ -89,8 +61,6 @@ def legals_dodo(grid: Grid, player: Player, directions) -> list[ActionDodo]:
                     if 0 <= r < len(grid) and 0 <= q < len(grid[0]):
                         if grid[neighbor[0]][neighbor[1]] == 0:
                             actions.add(((i, j), neighbor))
-                            temp_grid[r][q] = 4
-    #hexa.display_grid(hexa.grid_list_to_grid_tuple(temp_grid))
 
     return list(actions)
 
@@ -128,15 +98,14 @@ def strategy_random_dodo(grid: Grid, player: Player) -> Action:
 
 
 # Boucle de jeu Dodo
-def dodo(strategy_1: Strategy, strategy_2: Strategy, debug: bool = False) -> Score:
-    actual_grid: Grid = hexa.INIT_GRID
+def dodo(strategy_1: Strategy, strategy_2: Strategy, init_grid: Grid, debug: bool = False) -> Score:
+    actual_grid: Grid = init_grid
     current_player: Player = 1
     current_action: Action
     nb_iterations: int = 0
 
     while not (final_dodo(actual_grid) == 1 or final_dodo(actual_grid) == -1):
         nb_iterations += 1
-        print(f"Iteration \033[36m {nb_iterations}\033[0m.")
         if current_player == 1:
             current_action = strategy_1(actual_grid, current_player)
         else:
@@ -146,21 +115,16 @@ def dodo(strategy_1: Strategy, strategy_2: Strategy, debug: bool = False) -> Sco
             current_player = 2
         else:
             current_player = 1
-        hexa.display_grid(actual_grid)
+
+    print(f"Iteration \033[36m {nb_iterations}\033[0m.")
+    hexa.display_grid(actual_grid)
 
     return final_dodo(actual_grid)
 
 
 def main():
-    n = 7
-    init_grid = hexa.INIT_GRID
-    # hexa.display_neighbors(init_grid, 6, 0, UP_DIRECTIONS, n)
-    # print(legals_dodo(init_grid, player2, UP_DIRECTIONS))
-    # print(final_dodo(init_grid))
-    # print(legals_dodo(play_dodo(init_grid, player2, ((6, 0), (5, 1))), player2, UP_DIRECTIONS))
-
-    print(dodo(strategy_random_dodo, strategy_random_dodo, False))
-
+    init_grid = INIT_GRID
+    print(dodo(strategy_random_dodo, strategy_random_dodo, init_grid, False))
     pass
 
 
